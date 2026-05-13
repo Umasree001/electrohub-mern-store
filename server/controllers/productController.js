ca6bfaa4bc0f4ab31906ee89c7019524f58925fe
Umasree001 <umasreemokka2006@gmail.com>
2026-05-13 20:45:29 +0530

    Completed Product CRUD APIs

---
+++ server/controllers/productController.js
@@ -0,0 +1,111 @@
+import Product from "../models/Product.js";
+
+// GET all products
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// CREATE product
+export const createProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      price,
+      description,
+      image,
+      countInStock,
+    } = req.body;
+
+    const product = await Product.create({
+      name,
+      price,
+      description,
+      image,
+      countInStock,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// GET single product
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// UPDATE product
+export const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = req.body.name || product.name;
+      product.price = req.body.price || product.price;
+      product.description =
+        req.body.description || product.description;
+      product.image = req.body.image || product.image;
+      product.countInStock =
+        req.body.countInStock || product.countInStock;
+
+      const updatedProduct = await product.save();
+
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// DELETE product
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      await product.deleteOne();
+
+      res.json({
+        message: "Product removed",
+      });
+    } else {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
